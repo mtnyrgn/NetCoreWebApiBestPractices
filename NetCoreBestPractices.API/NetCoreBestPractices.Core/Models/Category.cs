a9122320b49ba0227d0f9da90b10c2cb4486f0f7
mtnyrgn <metinyorgun1996@gmail.com>
2020-11-24 01:24:40 +0300

    core models updated

--- NetCoreBestPractices.API/NetCoreBestPractices.Core/Models/Category.cs
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/Models/Category.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
 namespace NetCoreBestPractices.Core.Models
 {
     public class Category
     {
         public Category()
         {
+            Products = new Collection<Product>();
         }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public Collection<Product> Products { get; set; }
     }
 }
--- NetCoreBestPractices.API/NetCoreBestPractices.Core/Models/Product.cs
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/Models/Product.cs
@@ -3,8 +3,12 @@ namespace NetCoreBestPractices.Core.Models
 {
     public class Product
     {
-        public Product()
-        {
-        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public long Stock { get; set; }
+        public string InnerBarcode { get; set; }
+        public bool IsDeleted { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
