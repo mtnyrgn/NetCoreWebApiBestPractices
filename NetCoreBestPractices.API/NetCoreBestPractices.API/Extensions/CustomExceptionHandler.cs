66855939399c44c6a08de993c7431a29e97244d6
mtnyrgn <metinyorgun1996@gmail.com>
2020-11-26 15:28:43 +0300

    custom exception handler added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/Extensions/CustomExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using NetCoreBestPractices.API.DTO.Error;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.Extensions
+{
+    public static class CustomExceptionHandler
+    {
+        //Extension classlar ve metodlar static olmalı. Yazdığımız extensionları IApplicationBuilder üstüne yazacağız.Startuptaki IApplicationBuilder nesnesi üstünden buraya erişeceğiz.
+
+        public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (!(error is null))
+                    {
+                        var ex = error.Error;
+
+                        ErrorDto errorDto = new ErrorDto();
+                        errorDto.Status = 500;
+                        errorDto.Errors.Add(ex.Message);
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto));
+                    }
+                });
+            });
+        }
+    }
+}
