d872f60b9ad1989d4746bb6d59c1a383fe2f0799
mtnyrgn <metinyorgun1996@gmail.com>
2020-11-26 15:30:27 +0300

    product dto annotation added

--- NetCoreBestPractices.API/NetCoreBestPractices.API/DTO/ProductDto.cs
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/DTO/ProductDto.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace NetCoreBestPractices.API.DTO
 {
     public class ProductDto
     {
         public long Id { get; set; }
         public long CategoryId { get; set; }
+        [Required(ErrorMessage ="{0} alanı boş olamaz.")]
         public string Name { get; set; }
+        [Range(1, Double.MaxValue, ErrorMessage = "{0} alanı 1'den büyük bir değer olmalıdır. ")]
         public double Price { get; set; }
+        [Range(1,int.MaxValue,ErrorMessage ="{0} alanı 1'den büyük bir değer olmalıdır. ")]
         public long Stock { get; set; }
     }
 }
