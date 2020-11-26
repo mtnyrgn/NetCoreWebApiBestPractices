f2a1ba6f571be62a87dc700f540ba9b8d892b566
mtnyrgn <metinyorgun1996@gmail.com>
2020-11-26 15:28:25 +0300

    not found and validation filter added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/Filters/NotFoundFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NetCoreBestPractices.API.DTO.Error;
+using NetCoreBestPractices.Core;
+using NetCoreBestPractices.Core.Models;
+using NetCoreBestPractices.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.Filters
+{
+    public class NotFoundFilter : ActionFilterAttribute
+    {
+        private readonly IProductService _productService;
+        //private readonly IService<Product> _service;
+        public NotFoundFilter(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            long id = (long) context.ActionArguments.Values.FirstOrDefault();
+
+            var product = await _productService.GetByIdAsync(id);
+
+            if(!(product is null))
+            {
+                //Eğer yolladığım product id sistemde varsa isteği işlemeye devam edecek.
+                await next();
+            }
+            else
+            {
+                ErrorDto errorDto = new ErrorDto();
+
+                errorDto.Status = 404;
+                errorDto.Errors.Add($"Id'si {id} olan ürün veritabanında bulunamadı.");
+
+                context.Result = new NotFoundObjectResult(errorDto);
+            }
+        }
+
+    }
+}
---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/Filters/ValidationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NetCoreBestPractices.API.DTO.Error;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.Filters
+{
+    public class ValidationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                //Modelde herhangi bir hata varsa buraya girecek
+                ErrorDto error = new ErrorDto();
+
+                error.Status = 400; //client hatası olduğu için 400 kodu dönmeliyim.
+                IEnumerable<ModelError> modelErrors = context.ModelState.Values.SelectMany(s => s.Errors);
+
+                modelErrors.ToList().ForEach(err =>
+                {
+                    error.Errors.Add(err.ErrorMessage); //Birden fazla hata varsa listeye ekliyorum
+                });
+
+                context.Result = new BadRequestObjectResult(error);
+            }
+        }
+    }
+}
