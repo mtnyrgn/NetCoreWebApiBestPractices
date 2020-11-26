2840ec9a560fafff2bc2540347c9db0f9970a368
mtnyrgn <metinyorgun1996@gmail.com>
2020-11-26 15:29:50 +0300

    error dto added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/DTO/Error/ErrorDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.DTO.Error
+{
+    public class ErrorDto
+    {
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public List<string> Errors { get; set; }
+        public int Status { get; set; }
+
+    }
+}
