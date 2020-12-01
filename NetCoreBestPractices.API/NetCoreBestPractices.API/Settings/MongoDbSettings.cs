ef8f36013c7ed7d248f984cb9675a93ccec840a2
mtnyrgn <metinyorgun1996@gmail.com>
2020-12-02 00:13:00 +0300

    MongoDbSettings added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/Settings/MongoDbSettings.cs
@@ -0,0 +1,15 @@
+﻿using NetCoreBestPractices.Core.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.Settings
+{
+    public class MongoDbSettings : IMongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+    }
+}
