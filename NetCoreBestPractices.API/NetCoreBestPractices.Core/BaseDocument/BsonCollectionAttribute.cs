1e35857afad53449492ec60fb210fbdd4775d408
mtnyrgn <metinyorgun1996@gmail.com>
2020-12-01 23:29:10 +0300

    mongo collection base class added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/BaseDocument/BsonCollectionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBestPractices.Core.BaseDocument
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class BsonCollectionAttribute : Attribute
+    {
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+
+        public string CollectionName { get; set; 
+        }
+    }
+}
---
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/BaseDocument/MongoBaseDocument.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBestPractices.Core.BaseDocument
+{
+    public abstract class MongoBaseDocument //Document RDBMS'teki row ile aynı anlama sahip. Her document yani row oluştuğu esnada mongodbde ona bir objectId oluşturulur. Bu yüzden her rowda olması
+    { //gereken özellikler için bir baseDocument oluşturduk.
+        [BsonId] //Id olacağı söylendi.
+        [BsonRepresentation(BsonType.String)]
+        public ObjectId Id { get; set; } //MongoDb de primary key Object Id ile ifade ediliyor.
+        public DateTime CreatedTime => Id.CreationTime; //Oluşturma zamanını direk ObjectIdnin oluşturma zamanına eşitledim.
+    }
+}
