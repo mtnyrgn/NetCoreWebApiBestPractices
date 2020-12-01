d977a3c72a9823b3a1290c7d3ee635831a0c2c56
mtnyrgn <metinyorgun1996@gmail.com>
2020-12-02 00:11:35 +0300

    IMongoDbSettings added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/Settings/IMongoDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBestPractices.Core.Settings
+{
+    public interface IMongoDbSettings
+    {
+        //Mongodb bağlantısı için kullanacağım parametrelerimin interfaceini tanımladım. Interfacelerimi core katmanında tanımlamalıyım.(++abstraction)...
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+    }
+}
