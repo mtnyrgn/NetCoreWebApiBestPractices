58506e0048ecd69d1c81be6663b5c4b7f7189b6b
mtnyrgn <metinyorgun1996@gmail.com>
2020-12-01 23:29:25 +0300

    person collection added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/Collections/Person.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using NetCoreBestPractices.Core.BaseDocument;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBestPractices.Core.Documents
+{
+    [BsonCollection("persons")] //Mongo üzerinde tabloyu hangi isimle çağıracağımı belirttim
+    public class Person : MongoBaseDocument //Her tablom baseden kalıtım almalı.Yani objectId'yi( yada daha fazlasını) default olarak içermeli.
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
