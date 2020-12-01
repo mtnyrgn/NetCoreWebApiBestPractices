c50976e9fd1c334171ff45511823472271151b69
mtnyrgn <metinyorgun1996@gmail.com>
2020-12-02 00:11:10 +0300

    IMongoService name fixed

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.Core/Services/IMongoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.Core.Services
+{
+    public interface IMongoService<TDocument> where TDocument : class
+    {
+        Task<TDocument> GetByIdAsync(string id);
+        Task<IEnumerable<TDocument>> GetAllAsync();
+        Task<TDocument> AddAsync(TDocument entity);
+        Task<IEnumerable<TDocument>> AddRangeAsync(IEnumerable<TDocument> entities);
+        void Remove(string id);
+        TDocument Update(TDocument entity);
+    }
+}
