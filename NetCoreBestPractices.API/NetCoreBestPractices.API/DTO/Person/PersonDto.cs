ef8a0505e0a0f6f4418247a2aa244b2606a58a94
METİN YORGUN <myorgun@tpao.gov.tr>
2020-12-07 11:18:00 +0300

    person dto added

---
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/DTO/Person/PersonDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreBestPractices.API.DTO.Person
+{
+    public class PersonDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
