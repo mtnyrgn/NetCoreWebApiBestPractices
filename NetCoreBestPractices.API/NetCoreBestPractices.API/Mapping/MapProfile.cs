3cfa8a9ed20372d2203271e41b8f9d5cce18b29b
METİN YORGUN <myorgun@tpao.gov.tr>
2020-12-07 11:18:18 +0300

    person-person dto map profile added

--- NetCoreBestPractices.API/NetCoreBestPractices.API/Mapping/MapProfile.cs
+++ NetCoreBestPractices.API/NetCoreBestPractices.API/Mapping/MapProfile.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using AutoMapper;
 using NetCoreBestPractices.API.DTO;
+using NetCoreBestPractices.API.DTO.Person;
+using NetCoreBestPractices.Core.Documents;
 using NetCoreBestPractices.Core.Models;
 
 namespace NetCoreBestPractices.API.Mapping
@@ -17,10 +19,11 @@ namespace NetCoreBestPractices.API.Mapping
 
             CreateMap<Product, ProductDto>();
             CreateMap<ProductDto, Product>();
-
             CreateMap<Product, ProductWithCategoryDto>();
-
             CreateMap<ProductWithCategoryDto, Product>();
+
+            CreateMap<PersonDto, Person>();
+            CreateMap<Person, PersonDto>();
 
         }
     }
